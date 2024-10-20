675eb7766a3a535d130f073c97f4fb02c7ebc10b
KatzMatz <katz.matz.dev@gmail.com>
2024-10-20 12:31:31 +0900

    first commit

---
+++ shift_only.hs
@@ -0,0 +1,20 @@
+import Data.List (sort)
+
+-- ABC081B - Shift only
+-- https://atcoder.jp/contests/abs/tasks/abc081_b
+
+main :: IO ()
+main = do
+  _ <- readLn :: IO Int
+  a <- map read . words <$> getLine :: IO [Int]
+  putStrLn $ show $ solve a
+
+solve :: [Int] -> Int
+solve a =
+  let counts = map countDevide2 a
+   in head $ sort counts
+
+countDevide2 :: Int -> Int
+countDevide2 n
+  | mod n 2 == 0 = 1 + (countDevide2 $ n `div` 2)
+  | otherwise = 0
