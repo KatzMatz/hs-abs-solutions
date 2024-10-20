f9850e671afde94f45ed66ff455982196ab9bf29
KatzMatz <katz.matz.dev@gmail.com>
2024-10-20 12:12:28 +0900

    simplify

--- placing_marbles.hs
+++ placing_marbles.hs
@@ -4,10 +4,4 @@
 main :: IO ()
 main = do
   s <- getLine :: IO String
-  putStrLn $ show $ countElem s '1' 0
-
-countElem :: String -> Char -> Int -> Int
-countElem [] _ ans = ans
-countElem (x : xs) elem ans
-  | x == elem = countElem xs elem (ans + 1)
-  | otherwise = countElem xs elem ans
+  putStrLn $ show $ length $ filter (== '1') s
